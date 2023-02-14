cbf7d894ebc91444f3378bc67470b175be6bf0f9
Aditya Shinde <adityashinde4580@gmail.com>
2023-02-15 04:17:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/multiscreenapp/OrderActivity.kt
@@ -0,0 +1,17 @@
+package com.example.multiscreenapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_order.*
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+
+        val ordersOfCustomer = intent.getStringExtra(MainActivity.KEY)
+
+        tVOrder.text = " Order Placed \n" + ordersOfCustomer.toString()
+
+    }
+}
